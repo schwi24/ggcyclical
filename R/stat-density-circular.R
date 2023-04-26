dd551d6c20518141ce71d12ac66627177df0a66e
schwi24 <chris.schweingruber@bluewin.ch>
2023-04-26 16:29:52 +0200

    Rewrite 'stat_mode' so that it calculates the modes in 'stat_density' or 'stat_density_circular' directly. This also made 'stat_mode_circular' redundant and was therefore removed.

--- R/stat-density-circular.R
+++ R/stat-density-circular.R
@@ -39,18 +39,18 @@ stat_density_circular <- function(mapping = NULL, data = NULL,
 }
 
 
-StatDensityCircular <- ggproto(
+StatDensityCircular <- ggplot2::ggproto(
   "StatDensityCircular",
-  Stat,
+  ggplot2::Stat,
   
   required_aes = c("x|y"),
   
-  default_aes = aes(x = after_stat(density), y = after_stat(density), fill = NA, weight = NULL),
+  default_aes = ggplot2::aes(x = after_stat(density), y = after_stat(density), fill = NA, weight = NULL),
   
   dropped_aes = "weight",
   
   setup_params = function(self, data, params) {
-    params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = FALSE, main_is_continuous = TRUE)
+    params$flipped_aes <- ggplot2::has_flipped_aes(data, params, main_is_orthogonal = FALSE, main_is_continuous = TRUE)
     
     has_x <- !(is.null(data$x) && is.null(params$x))
     has_y <- !(is.null(data$y) && is.null(params$y))
@@ -168,165 +168,23 @@ compute_density_circular <- function(
       x = x, bw = bw, adjust = adjust, kernel = kernel, n = n, K = K
     )
     dens <- ggplot2:::reflect_density(
-      dens = dens, bounds = bounds, from = from, to = to
+      dens = dens, bounds = bounds, from = circular::circular(from * 2 * pi / period), to = circular::circular(to * 2 * pi / period)
     )
   } else {
     dens <- circular::density.circular(
       x = x, bw = bw, adjust = adjust, kernel = kernel, n = n, K = K,
-      from = from, to = to
+      from = circular::circular(from * 2 * pi / period), to = circular::circular(to * 2 * pi / period)
     )
   }
   
-  ggplot2:::data_frame0(
+  tibble::tibble(
     x = dens$x * period / (2 * pi),
     density = dens$y * 2 * pi / period,
     scaled = dens$y / max(dens$y, na.rm = TRUE),
     ndensity = dens$y / max(dens$y, na.rm = TRUE),
     count = dens$y * nx * 2 * pi / period,
     n = nx,
-    .size = length(dens$x)
-  )
-}
-
-stat_density_circular2 <- function(mapping = NULL, data = NULL,
-                                  geom = "area", position = "stack",
-                                  ...,
-                                  period = 2 * pi,
-                                  bw = "SJ",
-                                  adjust = 1,
-                                  kernel = "gaussian",
-                                  n = 512,
-                                  trim = FALSE,
-                                  na.rm = FALSE,
-                                  bounds = c(-Inf, Inf),
-                                  orientation = NA,
-                                  show.legend = NA,
-                                  inherit.aes = TRUE) {
-  
-  layer(
-    data = data,
-    mapping = mapping,
-    stat = StatDensityCircular2,
-    geom = geom,
-    position = position,
-    show.legend = show.legend,
-    inherit.aes = inherit.aes,
-    params = list(
-      period = period,
-      bw = bw,
-      adjust = adjust,
-      kernel = kernel,
-      n = n,
-      trim = trim,
-      na.rm = na.rm,
-      bounds = bounds,
-      orientation = orientation,
-      ...
-    )
-  )
-}
-
-StatDensityCircular2 <- ggproto("StatDensityCircular2", Stat,
-  
-  required_aes = c("x|y"),
-  
-  default_aes = aes(x = after_stat(density), y = after_stat(density), fill = NA, weight = NULL),
-  
-  dropped_aes = "weight",
-  
-  setup_params = function(self, data, params) {
-    params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = FALSE, main_is_continuous = TRUE)
-    
-    has_x <- !(is.null(data$x) && is.null(params$x))
-    has_y <- !(is.null(data$y) && is.null(params$y))
-    if (!has_x && !has_y) {
-      cli::cli_abort("{.fn {snake_class(self)}} requires an {.field x} or {.field y} aesthetic.")
-    }
-    
-    return(params)
-  },
-  
-  extra_params = c("na.rm", "orientation"),
-  
-  compute_group = function(
-    data, scales,
-    period = 2 * pi,
-    bw = "SJ", adjust = 1, kernel = "gaussian", n = 512, trim = FALSE,
-    na.rm = FALSE, bounds = c(-Inf, Inf),
-    flipped_aes = FALSE
-  ) {
-    
-    data <- flip_data(data, flipped_aes)
-    if (trim) {
-      range <- range(data$x, na.rm = TRUE)
-    } else {
-      range <- scales[[flipped_names(flipped_aes)$x]]$dimension()
-    }
-    
-    circular_density <- compute_density_circular2(
-      x = data$x, w = data$weight,
-      period = period, from = range[1], to = range[2],
-      bw = bw, adjust = adjust, kernel = kernel, n = n, bounds = bounds
-    )
-    circular_density$flipped_aes <- flipped_aes
-    flip_data(circular_density, flipped_aes)
-  }
-)
-
-compute_density_circular2 <- function(x, w, period = 2 * pi, from, to, bw = "SJ", adjust = 1, kernel = "gaussian", n = 512, bounds = c(-Inf, Inf)) {
-  
-  #Extend circular data ± period
-  nx <- 3 * length(x)
-  x <- c(x - period, x, x + period)
-  if (is.null(w)) {
-    w <- rep(1/nx, nx)
-  } else {
-    w <- rep(w, 3) / (3 * sum(w))
-  }
-  
-  # Adjust data points and weights to all fit inside bounds
-  sample_data <- ggplot2:::fit_data_to_bounds(bounds, x, w)
-  x <- sample_data$x
-  w <- sample_data$w
-  nx <- length(x)
-  
-  # if less than 2 data points, return data frame of NAs and a warning
-  if (nx < 6) {
-    cli::cli_warn("Groups with fewer than two data points have been dropped.")
-    return(
-      ggplot2:::data_frame0(
-        x = NA_real_,
-        density = NA_real_,
-        scaled = NA_real_,
-        ndensity = NA_real_,
-        n = NA_real_,
-        .size = 1
-      )
-    )
-  }
-  
-  # Decide whether to use boundary correction
-  if (any(is.finite(bounds))) {
-    dens <- stats::density(
-      x = x, weights = w, bw = bw, adjust = adjust, kernel = kernel, n = n
-    )
-    dens <- ggplot2:::reflect_density(
-      dens = dens, bounds = bounds, from = from, to = to
-    )
-  } else {
-    dens <- stats::density(
-      x = x, weights = w, bw = bw, adjust = adjust, kernel = kernel, n = n,
-      from = from, to = to
-    )
-  }
-  
-  ggplot2:::data_frame0(
-    x = dens$x,
-    density = 3 * dens$y,
-    scaled = dens$y / max(3 * dens$y, na.rm = TRUE),
-    ndensity = dens$y / max(3 * dens$y, na.rm = TRUE),
-    count = 3 * dens$y * nx,
-    n = nx / 3,
-    .size = length(dens$x)
+    .rows = length(dens$x),
+    .name_repair = "minimal"
   )
 }
--- R/stat-mode.R
+++ R/stat-mode.R
@@ -1,7 +1,7 @@
 stat_mode <- function(mapping = NULL, data = NULL,
                                geom = "point", position = "stack",
                                ...,
-                               period = 2 * pi,
+                               mode = "peak",
                                bw = "nrd0",
                                adjust = 1,
                                kernel = "gaussian",
@@ -22,7 +22,7 @@ stat_mode <- function(mapping = NULL, data = NULL,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
-      period = period,
+      mode = mode,
       bw = bw,
       adjust = adjust,
       kernel = kernel,
@@ -37,18 +37,18 @@ stat_mode <- function(mapping = NULL, data = NULL,
 }
 
 
-StatMode <- ggproto(
+StatMode <- ggplot2::ggproto(
   "StatMode",
-  Stat,
+  ggplot2::Stat,
   
   required_aes = c("x|y"),
   
-  default_aes = aes(x = after_stat(density), y = after_stat(density), fill = NA, weight = NULL),
+  default_aes = ggplot2::aes(x = after_stat(density), y = after_stat(density), fill = NA, weight = NULL),
   
   dropped_aes = "weight",
   
   setup_params = function(self, data, params) {
-    params$flipped_aes <- has_flipped_aes(data, params, main_is_orthogonal = FALSE, main_is_continuous = TRUE)
+    params$flipped_aes <- ggplot2::has_flipped_aes(data, params, main_is_orthogonal = FALSE, main_is_continuous = TRUE)
     
     has_x <- !(is.null(data$x) && is.null(params$x))
     has_y <- !(is.null(data$y) && is.null(params$y))
@@ -69,88 +69,45 @@ StatMode <- ggproto(
     flipped_aes = FALSE
   ) {
     
-    data <- flip_data(data, flipped_aes)
+    data <- ggplot2::flip_data(data, flipped_aes)
     if (trim) {
       range <- range(data$x, na.rm = TRUE)
     } else {
       range <- scales[[flipped_names(flipped_aes)$x]]$dimension()
     }
     
-    modal <- compute_mode(
-      x = data$x, w = data$weight,
-      mode = mode, from = range[1], to = range[2],
+    modal <- ggplot2:::compute_density(
+      data$x, data$weight, from = range[1], to = range[2],
       bw = bw, adjust = adjust, kernel = kernel, n = n, bounds = bounds
     )
+    modal <- compute_mode(data = modal, mode = mode)
+
     modal$flipped_aes <- flipped_aes
-    flip_data(modal, flipped_aes)
+    ggplot2::flip_data(modal, flipped_aes)
   }
 )
 
-compute_mode <- function(x, w, mode = "peak", from, to, bw = "nrd0", adjust = 1, kernel = "gaussian", n = 512, bounds = c(-Inf, Inf)) {
-  
-  nx <- length(x)
-  if (is.null(w)) {
-    w <- rep(1/nx, nx)
-  } else {
-    w <- w / sum(w)
-  }
-  
-  # Adjust data points and weights to all fit inside bounds
-  sample_data <- ggplot2:::fit_data_to_bounds(bounds, x, w)
-  x <- sample_data$x
-  w <- sample_data$w
-  nx <- length(x)
-  
-  # if less than 2 data points, return data frame of NAs and a warning
-  if (nx < 2) {
-    cli::cli_warn("Groups with fewer than two data points have been dropped.")
-    return(
-      ggplot2:::data_frame0(
-        x = NA_real_,
-        density = NA_real_,
-        scaled = NA_real_,
-        ndensity = NA_real_,
-        n = NA_real_,
-        .size = 1
-      )
-    )
-  }
-  # if selected mode invalid, choose default and return warning
-  if (!(mode %in% c("peak", "trough", "both"))) {
-    cli::cli_warn("Mode defaulted to peak because of invalid value.")
-    mode <- "peak"
-  }
-  
-  # Decide whether to use boundary correction
-  if (any(is.finite(bounds))) {
-    dens <- stats::density(
-      x = x, weights = w, bw = bw, adjust = adjust, kernel = kernel, n = n,
-    )
-    dens <- ggplot2:::reflect_density(
-      dens = dens, bounds = bounds, from = from, to = to
-    )
-  } else {
-    dens <- stats::density(
-      x = x, weights = w, bw = bw, adjust = adjust, kernel = kernel, n = n,
-      from = from, to = to
-    )
-  }
-  
-  # Extract coordinates of modes
-  sel <- switch(
+compute_mode <- function(data, mode) {
+
+  modal_positions <- switch(
     mode,
-    "peak" = which(diff(sign(diff(dens$y))) < 0) + 1,
-    "trough" = which(diff(sign(diff(dens$y))) > 0) + 1,
-    "both" = c(which(diff(sign(diff(dens$y))) < 0) + 1, which(diff(sign(diff(dens$y))) > 0) + 1)
+    "peak" = which(diff(sign(diff(data$density))) < 0) + 1,
+    "trough" = which(diff(sign(diff(data$density))) > 0) + 1,
+    "both" = c(
+      which(diff(sign(diff(data$density))) < 0) + 1,
+      which(diff(sign(diff(data$density))) > 0) + 1
+    )
   )
-  
-  ggplot2:::data_frame0(
-    x = dens$x[sel],
-    density = dens$y[sel],
-    scaled = dens$y[sel] / max(dens$y, na.rm = TRUE),
-    ndensity = dens$y[sel] / max(dens$y, na.rm = TRUE),
-    count = dens$y[sel] * nx,
-    n = nx,
-    .size = length(dens$x[sel])
+
+  tibble::tibble(
+    x = data$x[modal_positions],
+    density = data$density[modal_positions],
+    scaled = data$scaled[modal_positions],
+    ndensity = data$ndensity[modal_positions],
+    count = data$count[modal_positions],
+    n = data$n[modal_positions],
+    .rows = length(data$x[modal_positions]),
+    .name_repair = "minimal"
   )
+
 }
